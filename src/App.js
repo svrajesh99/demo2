a36414f63c624b9f515821daff01a0f0640b3a36
venkatapraveen698 <128704361+venkatapraveen698@users.noreply.github.com>
2023-05-22 19:10:10 +0530

    new demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Menu from './Components/Menu';
-import Imageslider from './Component2/images';
 function App() {
   return (
     <div className="App">
       <Menu/>
-      {/* <Imageslider/> */}
     </div>
   );
 }
